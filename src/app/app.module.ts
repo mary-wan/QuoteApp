34eb55d676819f2135233e950110e444d2d9ab79
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-07 22:15:09 +0300

    add quote component and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
