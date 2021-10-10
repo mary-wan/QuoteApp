b9d4a875a6ce5c6b524e17a0062d8049ca54d9e7
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-10 19:18:20 +0300

    add function for highest voted quote

--- src/app/highest-vote.directive.ts
+++ src/app/highest-vote.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighestVote]'
 })
 export class HighestVoteDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef){
+    this.elem.nativeElement.style.backgroundColor='cyan';
+}
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,29 +8,18 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   constructor() { }
-  highestVotes: number = 0; // highestVotes Starts at Zero for Every Posted Quote
-  highestQuote:any = 0;
-  highestUpvote() {
-    for (var i = 0; i < this.quotes.length; i++) { // Loop through Current Posted quotes
-       if (this.quotes[i].upvotes > this.highestVotes) { // If noOfVotes for current looped quote is higher than the current highestVotes
-          this.highestVotes = this.quotes[i].upvotes; // Assign the new noOfVotes value to highestVotes
-          this.highestQuote = i; // Take the index of the quote with the highest noOfVotes and assign its index to highestQuote
-         console.log(this.highestQuote);
-         
-       }
-       
-    }
- }
-
+  highVote: number = 0; // Starts at Zero for Every Posted Quote
+  favQuote:number = 0;
+  
   quotes: Quote[]= [
     new Quote (0,"Kiki ","Napoleon Hill","If you cannot do great things, do small things in a great way.",1,0,new Date(2019,4,3)),
-    new Quote (1,"Mary Njenga","Eleanor Roosevelt","The future belongs to those who believe in the beauty of their dreams.",2,0,new Date(2016,6,5))
+    // new Quote (1,"Mary Njenga","Eleanor Roosevelt","The future belongs to those who believe in the beauty of their dreams.",0,0,new Date(2016,6,5))
   ]
   addNewQuote(quote: Quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     quote.date = new Date(quote.date);
-    this.quotes.unshift(quote);
+    this.quotes.push(quote);
   }
   quoteDelete(isDelete:boolean, index:number){
     if(isDelete){
@@ -50,7 +39,17 @@ export class QuoteComponent implements OnInit {
       this.quotes[index].downvotes +=1;
     }
   }
- 
+  highestUpvote(){
+    for (var i = 0; i < this.quotes.length; i++) { // Loop through Current Posted quotes
+       if (this.quotes[i].upvotes > this.highVote) { // If noOfVotes for current looped quote is higher than the current highestVotes
+          this.highVote = this.quotes[i].upvotes; // Assign the new noOfVotes value to highestVotes
+          this.favQuote = i; // Take the index of the quote with the highest noOfVotes and assign its index to highestQuote
+       }
+       
+    }
+
+ }
+
   
 
   ngOnInit(): void {
