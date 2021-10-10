37c5bff8aab11581db9d051b6f4c120d2a3d8a49
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-10 11:57:34 +0300

    add placeholder

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,8 +10,8 @@ export class QuoteComponent implements OnInit {
   constructor() { }
 
   quotes: Quote[]= [
-    new Quote (0,"Kiki ","Napoleon Hill","If you cannot do great things, do small things in a great way.",1,0,new Date(2019,4,3)),
-    new Quote (1,"Mary Njenga","Eleanor Roosevelt","The future belongs to those who believe in the beauty of their dreams.",0,0,new Date(2016,6,5))
+    new Quote (0,"Kiki ","Napoleon Hill","If you cannot do great things, do small things in a great way.",2,0,new Date(2019,4,3)),
+    // new Quote (1,"Mary Njenga","Eleanor Roosevelt","The future belongs to those who believe in the beauty of their dreams.",0,0,new Date(2016,6,5))
   ]
   addNewQuote(quote: Quote){
     let quoteLength = this.quotes.length;
